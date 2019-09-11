cf5d1b0ad59183d81eea53077282160191ffc779
Nikhil Vallore <Nikhil_Vallore@epam.com>
2019-09-11 16:49:31 +0530

    ReserveComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReservationDashboardComponent } from './components/Reservation/reservation-dashboard/reservation-dashboard.component';
 import { ReservebookComponent } from './components/Reservation/reservebook/reservebook.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,11 @@ import { ReservebookComponent } from './components/Reservation/reservebook/reser
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/Reservation/reservebook/reservebook.component.ts
+++ src/app/components/Reservation/reservebook/reservebook.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../reservation.service';
 
 @Component({
   selector: 'app-reservebook',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservebookComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private reservationService: ReservationService) { }
+  books: any = [];
+  myDate: Date;
+  minDate: Date;
+  maxDate: Date;
   ngOnInit() {
+    this.myDate = new Date();
+    this.minDate = new Date(
+      this.myDate.getFullYear(),
+      this.myDate.getMonth(),
+      this.myDate.getDate());
+    this.maxDate = new Date(
+      this.myDate.getFullYear(),
+      this.myDate.getMonth() ,
+      this.myDate.getDate() + 10);
+    this.books = this.reservationService.getReservedBooks('user1');
+    console.log(this.books);
   }
+
 
 }
